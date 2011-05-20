5598d72de3560ae67739d9c4c66f2a25b4fa6bbf
Francisco Souza <francisco@franciscosouza.net>
2011-05-20 01:56:45 -0300

    Added solution for problem 2

---
+++ problem_002/2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Fibonacci(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	return Fibonacci(n - 1) + Fibonacci(n - 2)
+}
+
+func main() {
+	sum := 0
+	fibo := 0
+	for i := 0; fibo <= 4000000; i++ {
+		fibo = Fibonacci(i)
+		fmt.Println(fibo)
+		if fibo % 2 == 0 {
+			sum += fibo
+		}
+	}
+	fmt.Println(sum)
+}
