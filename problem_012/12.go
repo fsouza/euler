32c65a8ab9cba2384462b75513f706f0fd5ffbbc
Francisco Souza <francisco@franciscosouza.net>
2011-05-28 22:26:58 -0300

    Implemented function that get all prime factors of a number

--- problem_012/12.go
+++ problem_012/12.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"container/vector"
+)
+
+func GetPrimeFactors(number int) *vector.IntVector {
+	numbers := new(vector.IntVector)
+
+	primesChannel := GetPrimes()
+	for  {
+		prime := <-primesChannel
+
+		if number < prime {
+			break
+		}
+
+		if number % prime == 0 {
+			numbers.Push(prime)
+		}
+	}
+
+	return numbers
+}
+
 func main() {
+
 }
--- problem_012/12_test.go
+++ problem_012/12_test.go
@@ -1,7 +1,32 @@
 package main
 
 import (
+	"container/vector"
 	"testing"
 )
 
+func AreTwoIntVectorEquals(vector1, vector2 *vector.IntVector) bool {
+	if vector1.Len() != vector2.Len() {
+		return false
+	}
 
+	for i := 0; i < vector1.Len(); i++ {
+		if vector1.At(i) != vector2.At(i) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestGetPrimeFactorsOfANumber(t *testing.T) {
+	expected := new(vector.IntVector)
+	for _, integer := range []int{2, 3, 5} {
+		expected.Push(integer)
+	}
+
+	got := GetPrimeFactors(90)
+	if !AreTwoIntVectorEquals(expected, got) {
+		t.Errorf("Expected %q. Got %q", expected, got)
+	}
+}
