f1f7417e3245347ad3fe2e3a96343c6625bf61cb
Francisco Souza <francisco@franciscosouza.net>
2011-06-23 21:39:47 -0300

    Function that finds the length of the chain

--- problem_014/14.go
+++ problem_014/14.go
@@ -1,1 +1,15 @@
 package main
+
+func FindChainLength(number int) int {
+	counter := 1
+	for number != 1 {
+		counter++
+		if number % 2 == 0 {
+			number /= 2
+		} else {
+			number = number * 3 + 1
+		}
+	}
+
+	return counter
+}
--- problem_014/14_test.go
+++ problem_014/14_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestFindChainLengthForNumber(t *testing.T) {
+	expected := 10
+	got := FindChainLength(13)
+	if expected != got {
+		t.Errorf("%d != %d", expected, got)
+	}
+}
+
