aa07d08259f9214fc0469ab134a2bc5044c7ca65
Francisco Souza <francisco@franciscosouza.net>
2011-06-25 00:27:49 -0300

    Calculating a element at a given row and column in pascal's triangle

--- problem_015/15.go
+++ problem_015/15.go
@@ -1,1 +1,19 @@
 package main
+
+type PascalRow []int
+
+func GetPascalsTriangleCell(row, column int) int {
+	entireRow := make([]int, row + 1)
+	entireRow[0] = 1
+
+	numerator := row
+	denominator := 1
+
+	for counter := 1; counter < row + 1; counter++ {
+		entireRow[counter] = entireRow[counter - 1] * numerator/denominator
+		numerator--
+		denominator++
+	}
+
+	return entireRow[column]
+}
--- problem_015/15_test.go
+++ problem_015/15_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestGetCellOnPascalTriangle(t *testing.T) {
+	expected := 6
+	got := GetPascalsTriangleCell(4, 2)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %d.\nGot: %d.", expected, got)
+	}
+}
+
