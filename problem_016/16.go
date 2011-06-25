ae704d3ee92e78b1e5e00fda5726c818469289e2
Francisco Souza <francisco@franciscosouza.net>
2011-06-25 00:48:26 -0300

    Function that sum digits of a number

--- problem_016/16.go
+++ problem_016/16.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"strconv"
+)
+
+func SumDigits(number uint64) uint64 {
+	var sum uint64
+
+	digits := strconv.Uitoa64(number)
+	for _, digit := range digits {
+		sum += uint64(digit - '0')
+	}
+
+	return sum
+}
--- problem_016/16_test.go
+++ problem_016/16_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestSumDigitsOfANumber(t *testing.T) {
+	expected := uint64(9)
+	got := SumDigits(27)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %d.\nGot: %d.", expected, got)
+	}
+}
+
