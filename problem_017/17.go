901df45fcda5e5ccba75a7569916eb99a0c52320
Francisco Souza <francisco@franciscosouza.net>
2011-06-25 12:42:52 -0300

    First basic implementation of number writing

--- problem_017/17.go
+++ problem_017/17.go
@@ -1,1 +1,17 @@
 package main
+
+var unitiesMap = map[int]string{
+	1:"one",
+	2:"two",
+	3:"three",
+	4:"four",
+	5:"five",
+	6:"six",
+	7:"seven",
+	8:"eight",
+	9:"nine",
+}
+
+func WriteNumber(number int) string {
+	return unitiesMap[number]
+}
--- problem_017/17_test.go
+++ problem_017/17_test.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"testing"
+)
+
+type Problem17Test struct {
+	in int
+	out string
+}
+
+var Problem17Tests = []Problem17Test{
+	Problem17Test{1, "one"},
+	Problem17Test{2, "two"},
+	Problem17Test{3, "three"},
+	Problem17Test{4, "four"},
+	Problem17Test{5, "five"},
+	Problem17Test{6, "six"},
+	Problem17Test{7, "seven"},
+	Problem17Test{8, "eight"},
+	Problem17Test{9, "nine"},
+}
+
+func TestProblem17(t *testing.T) {
+	for _, v := range Problem17Tests {
+		expected := v.out
+		got := WriteNumber(v.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+		}
+	}
+}
+
