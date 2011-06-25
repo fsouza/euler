f2bd187b430f0852e5efaf6c644a83535497e077
Francisco Souza <francisco@franciscosouza.net>
2011-06-25 01:55:10 -0300

    Simplified run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-if [ $1 -lt 10 ];
-then
-    dir_name="problem_00$1"
-else
-    if [ $1 -lt 100 ];
-    then
-        dir_name="problem_0$1"
-    else
-        dir_name="problem_$1"
-    fi
-fi
-
+dir_name=`printf "problem_%03d" $1`
 cd $dir_name && 6g $1.go && 6l $1.6 && ./6.out && make clean
