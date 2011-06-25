43e215e76b9a4f30d4e6f1c449a145c3974a7737
Francisco Souza <francisco@franciscosouza.net>
2011-06-25 00:44:41 -0300

    Updated new_problem script

--- scripts/new_problem.py
+++ scripts/new_problem.py
@@ -1,12 +1,39 @@
 import os
 import sys
 
+MAKEFILE_TEMPLATE = r'''
+include $(GOROOT)/src/Make.inc
+
+TARG=main
+GOFILES=\
+		%(problem)d.go
+
+include $(GOROOT)/src/Make.pkg
+'''
+
+def fill_file(path, problem):
+    if path.endswith('.go'):
+        content = "package main"
+    else:
+        content = MAKEFILE_TEMPLATE % { 'problem' : problem }
+
+    fp = open(path, 'w')
+    fp.write(content)
+    fp.close()
+
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if __name__ == '__main__':
     problem = int(sys.argv[1])
     print 'Generating structure for problem %d... ' % problem,
+
     problem_dir = os.path.join(ROOT_DIR, 'problem_%03d' % problem)
-    file_path = os.path.join(problem_dir, '%d.go' % problem)
     os.mkdir(problem_dir)
-    open(file_path, 'w').close()
-    print 'Done.'
+
+    files = "%(problem)d.go %(problem)d_test.go Makefile" % { 'problem' : problem }
+    files = files.split()
+    file_paths = [os.path.join(problem_dir, filename) for filename in files]
+
+    for path in file_paths:
+        fill_file(path, problem)
+
+    print 'done.'
