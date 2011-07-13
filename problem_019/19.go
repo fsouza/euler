48b61a3ed8578f7a76ab8a044b9bffcbb4dd64a0
Francisco Souza <francisco@franciscosouza.net>
2011-07-13 11:38:51 -0300

    Doomsday algorithm

--- problem_019/19.go
+++ problem_019/19.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"strconv"
+)
+
+func FindDayOfWeek(year, month, day int) int {
+	var centuryCode int
+	yearString := strconv.Itoa(year)
+	lastYearDigits, _ := strconv.Atoi(yearString[2:])
+
+	monthCodes := []int{ 1, 4, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6 }
+
+	if year < 2000 {
+		centuryCode = 0
+	} else {
+		centuryCode = 6
+	}
+
+	yearCode := (centuryCode + lastYearDigits + (lastYearDigits / 4)) % 7
+	return (yearCode + monthCodes[month - 1] + day) % 7
+}
--- problem_019/19_test.go
+++ problem_019/19_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestFindDayOfWeek(t *testing.T) {
+	expected := 5 // 5 == thursday
+	got := FindDayOfWeek(1989, 2, 16)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
