0577a3f5cbab9fb52e6289a5587678ef65484be9
Francisco Souza <francisco@franciscosouza.net>
2011-07-13 23:11:26 -0300

    sum of values for a name

--- problem_022/22.go
+++ problem_022/22.go
@@ -1,1 +1,9 @@
 package main
+
+func CountLetterNumbers(word string) (sum int) {
+	for _, c := range word {
+		sum += c - 64
+	}
+
+	return
+}
--- problem_022/22_test.go
+++ problem_022/22_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestCanFindTheSumOfAWord(t *testing.T) {
+	expected := 53
+	got := CountLetterNumbers("COLIN")
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
