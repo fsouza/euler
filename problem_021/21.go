c173cb21d764adcde15280af44876f100e5b321f
Francisco Souza <francisco@franciscosouza.net>
2011-07-13 22:37:10 -0300

    finding sum of factors

--- problem_021/21.go
+++ problem_021/21.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"math"
+)
+
+func SumOfFactors(number uint) uint {
+	sum := uint(1)
+	sqrt := uint(math.Sqrt(float64(number)))
+
+	for i := uint(2); i < sqrt + 1; i++ {
+		if number % i == 0 {
+			sum += i + number / i
+		}
+	}
+
+	return sum
+}
--- problem_021/21_test.go
+++ problem_021/21_test.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"testing"
+)
+
+type FactorsTest struct {
+	in, out uint
+}
+
+var	FactorsTests = []FactorsTest{
+	FactorsTest{ 220, 284 },
+	FactorsTest{ 284, 220 },
+}
+
+func TestFindFactors(t *testing.T) {
+	for _, test := range FactorsTests {
+		expected := test.out
+		got := SumOfFactors(test.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+		}
+	}
+}
+
