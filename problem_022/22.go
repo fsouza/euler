2cac4f7e3a754ea9564c6442b1673c57bf810604
Francisco Souza <francisco@franciscosouza.net>
2011-07-14 13:32:50 -0300

    Stopped using big package

--- problem_022/22.go
+++ problem_022/22.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-	"big"
 	"fmt"
 	"io/ioutil"
 	"sort"
 	"strings"
 )
 
-func CountLetterNumbers(word string) *big.Int {
-	sum := big.NewInt(0)
-
+func CountLetterNumbers(word string) (sum int64) {
 	for _, c := range word {
-		sum.Add(sum, big.NewInt(int64(c - 'A' + 1)))
+		sum += int64(c - 'A' + 1)
 	}
 
-	return sum
+	return
 }
 
 func main() {
-	totalScore := big.NewInt(0)
+	totalScore := int64(0)
 
 	if bytes, err := ioutil.ReadFile("names.txt"); err == nil {
 		names := strings.Split(string(bytes), ",")
 		sort.Strings(names)
 		for i, name := range names {
 			wordScore := CountLetterNumbers(name)
-			wordScore.Mul(wordScore, big.NewInt(int64(i + 1)))
-			totalScore.Add(totalScore, wordScore)
+			wordScore *= int64(i + 1)
+			totalScore += wordScore
 		}
 	}
 	
--- problem_022/22_test.go
+++ problem_022/22_test.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"big"
 	"testing"
 )
 
 func TestCanFindTheSumOfAWord(t *testing.T) {
-	expected := big.NewInt(53)
+	expected := int64(53)
 	got := CountLetterNumbers("COLIN")
-	if expected.Int64() != got.Int64() {
+	if expected != got {
 		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
 	}
 }
