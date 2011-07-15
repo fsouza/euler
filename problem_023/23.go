3e863261e328af4caa6c0411e0e187c7dacfcaf0
Francisco Souza <francisco@franciscosouza.net>
2011-07-14 21:39:27 -0300

    Function that cheks if a number is abundant

--- problem_023/23.go
+++ problem_023/23.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"math"
+)
+
+// Stole from problem 21
+func SumOfFactors(number uint) uint {
+	sum := uint(1)
+	sqrt := uint(math.Sqrt(float64(number)))
+
+	for i := uint(2); i < sqrt + 1; i++ {
+		if number % i == 0 {
+			sum += i + number / i
+		}
+	}
+
+	return sum
+}
+
+func IsAbundant(number uint) bool {
+	return SumOfFactors(number) > number
+}
--- problem_023/23_test.go
+++ problem_023/23_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"testing"
+)
+
+type IsAbundantTest struct {
+	in uint
+	out bool
+}
+
+var IsAbundantTests = []IsAbundantTest{
+	IsAbundantTest{ 12, true },
+	IsAbundantTest{ 15, false },
+	IsAbundantTest{ 24, true },
+}
+
+func TestCanCheckIfANumberIsAbundant(t *testing.T) {
+	for _, test := range IsAbundantTests {
+		expected := test.out
+		got := IsAbundant(test.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+		}
+	}
+}
+
