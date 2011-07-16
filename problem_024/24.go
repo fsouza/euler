b087a8689567255fc223c93a9b9afd295b30c1ea
Francisco Souza <francisco@franciscosouza.net>
2011-07-16 19:26:19 -0300

    Starting to work on permutations generation

--- problem_024/24.go
+++ problem_024/24.go
@@ -1,1 +1,16 @@
 package main
+
+func Factorial(number int) int {
+	factorial := int(1)
+
+	for i := number; i >= 1; i-- {
+		factorial *= i
+	}
+
+	return factorial
+}
+
+func GeneratePermutations(numbers ...int) []string {
+	permutations := make([]string, Factorial(len(numbers)))
+	return permutations
+}
--- problem_024/24_test.go
+++ problem_024/24_test.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"testing"
+)
+
+func AssertAreSlicesEqual(expected, got []string, t *testing.T) {
+	if len(expected) != len(got) {
+		t.Errorf("Expected: %v\nGot: %v", expected, got)
+	} else {
+		for i := 0; i < len(expected); i++ {
+			if !t.Failed() && expected[i] != got[i] {
+				t.Errorf("Expected: %v\nGot: %v", expected, got)
+			}
+		}
+	}
+}
+
+func TestGenerateAllPermutations(t *testing.T) {
+	expected := []string{ "012", "021", "102", "120", "201", "210", }
+	got := GeneratePermutations(0, 1, 2)
+	AssertAreSlicesEqual(expected, got, t)
+}
+
+func TestFactorial(t *testing.T) {
+	expected := 120
+	got := Factorial(5)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
