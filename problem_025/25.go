61aed2aa5ac8e44b5b1bb95e6a8481ae8b70f253
Francisco Souza <francisco@franciscosouza.net>
2011-07-16 20:17:51 -0300

    implemented fibonacci sequence (interactive way)

--- problem_025/25.go
+++ problem_025/25.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"big"
+)
+
+func Fibonacci(number int64) *big.Int {
+	first, second := big.NewInt(0), big.NewInt(1)
+
+	for i := number; i > 0; i-- {
+		first, second = second, first.Add(first, second)
+	}
+
+	return first
+}
--- problem_025/25_test.go
+++ problem_025/25_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"big"
+	"testing"
+)
+
+type FibonacciTest struct {
+	in int64
+	out *big.Int
+}
+
+var FibonacciTests = []FibonacciTest{
+	FibonacciTest{ 3, big.NewInt(2) },
+	FibonacciTest{ 7, big.NewInt(13) },
+	FibonacciTest{ 9, big.NewInt(34) },
+}
+
+func TestFibonacci(t *testing.T) {
+	for _, test := range FibonacciTests {
+		got := Fibonacci(test.in)
+		if test.out.Cmp(got) != 0 {
+			t.Errorf("Assertion error. Expected: %v\nGot: %v", test.out, got)
+		}
+	}
+}
