44a7598c0e250c819880fa061c45be2fe7fca3b4
Francisco Souza <francisco@franciscosouza.net>
2011-07-17 16:02:54 -0300

    Finding longest recurring

--- problem_026/26.go
+++ problem_026/26.go
@@ -1,12 +1,51 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+type Slice []int
 
-func ExtractDecimal(number float64) string {
-	out := fmt.Sprintf("%v", number)
-	decimal := strings.Split(out, ".")[1]
-	return decimal
+func (s Slice) IsElementPresent(number int) bool {
+	for _, element := range []int(s) {
+		if number == element {
+			return true
+		}
+	}
+
+	return false
+}
+
+func prepareNumerator(numerator, denominator int) int {
+	for numerator < denominator {
+		numerator *= 10
+	}
+
+	return numerator
+}
+
+func FindLongestRecurring(limit int) (number int) {
+	greater := 0
+
+	for i := 2; i < limit; i++ {
+		visited := make(Slice, 0)
+		numerator := prepareNumerator(1, i)
+
+		done := false
+		remainders := 0
+		for !done {
+			numerator = numerator % i
+
+			if numerator == 0 || visited.IsElementPresent(numerator) {
+				done = true
+			} else {
+				visited = append(visited, numerator)
+				numerator = prepareNumerator(numerator, i)
+			}
+
+			remainders++
+		}
+
+		if remainders > greater {
+			greater = remainders
+			number = i
+		}
+	}
+	return
 }
--- problem_026/26_test.go
+++ problem_026/26_test.go
@@ -4,25 +4,11 @@ import (
 	"testing"
 )
 
-type ExtractDecimalTest struct {
-	in float64
-	out string
-}
-
-var ExtractDecimalTests = []ExtractDecimalTest{
-	ExtractDecimalTest{ 0.1240, "124" },
-	ExtractDecimalTest{ 5.24565, "24565" },
-	ExtractDecimalTest{ 5.245655627147, "245655627147" },
-}
-
-func TestExtractDecimal(t *testing.T) {
-	for _, test := range ExtractDecimalTests {
-		expected := test.out
-		got := ExtractDecimal(test.in)
-		if expected != got {
-			t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
-		}
+func TestFindLongestRecurring(t *testing.T) {
+	expected := 7
+	got := FindLongestRecurring(10)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
 	}
 }
-
 
