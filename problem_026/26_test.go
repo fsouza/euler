c1ceb154877e1fb3095ca0c64dd992b3ee0b52cb
Francisco Souza <francisco@franciscosouza.net>
2011-07-17 16:03:32 -0300

    Fixed test

--- problem_026/26_test.go
+++ problem_026/26_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestFindLongestRecurring(t *testing.T) {
 	expected := 7
-	got := FindLongestRecurring(10)
+	got := FindLongestRecurring(11)
 	if expected != got {
 		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
 	}
