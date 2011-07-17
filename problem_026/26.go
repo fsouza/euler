6c438aee28a55fa59ae1a4149700beaf2daaf6ae
Francisco Souza <francisco@franciscosouza.net>
2011-07-17 15:42:17 -0300

    Function to extract decimal part from a number

--- problem_026/26.go
+++ problem_026/26.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ExtractDecimal(number float64) string {
+	out := fmt.Sprintf("%v", number)
+	decimal := strings.Split(out, ".")[1]
+	return decimal
+}
--- problem_026/26_test.go
+++ problem_026/26_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"testing"
+)
+
+type ExtractDecimalTest struct {
+	in float64
+	out string
+}
+
+var ExtractDecimalTests = []ExtractDecimalTest{
+	ExtractDecimalTest{ 0.1240, "124" },
+	ExtractDecimalTest{ 5.24565, "24565" },
+	ExtractDecimalTest{ 5.245655627147, "245655627147" },
+}
+
+func TestExtractDecimal(t *testing.T) {
+	for _, test := range ExtractDecimalTests {
+		expected := test.out
+		got := ExtractDecimal(test.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+		}
+	}
+}
+
+
