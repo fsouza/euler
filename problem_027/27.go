542775129cdeaeeb81a8b7b58892d3c9037998cb
Francisco Souza <francisco@franciscosouza.net>
2011-07-17 20:21:08 -0300

    Building a slice of primes from the channel

--- problem_027/27.go
+++ problem_027/27.go
@@ -1,1 +1,16 @@
 package main
+
+func BuildSliceOfPrimes(limit int) []uint {
+	primesCh := GetPrimes(float64(limit))
+	primes := make([]uint, 0)
+
+	for prime := range primesCh {
+		if prime == 0 {
+			break
+		}
+
+		primes = append(primes, prime)
+	}
+
+	return primes
+}
--- problem_027/27_test.go
+++ problem_027/27_test.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"testing"
+)
+
+func AssertAreSliceEqual(expected, got []uint, t *testing.T) {
+	if len(expected) != len(got) {
+		t.Errorf("Assertion error.\nExpected: %v\nGot:%v", expected, got)
+	} else {
+		for i := 0; i < len(expected); i++ {
+			if expected[i] != got[i] {
+				t.Errorf("Assertion error.\nExpected: %v\nGot:%v", expected, got)
+			}
+		}
+	}
+}
+
+func TestBuildSliceOfPrimes(t *testing.T) {
+	expected := []uint{ 2, 3, 5, 7, }
+	got := BuildSliceOfPrimes(10)
+	AssertAreSliceEqual(expected, got, t)
+}
+
