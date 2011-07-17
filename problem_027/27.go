e252f9158404b820b3df1f9598c8eca0f3101a93
Francisco Souza <francisco@franciscosouza.net>
2011-07-17 20:52:44 -0300

    Solution for problem 27

--- problem_027/27.go
+++ problem_027/27.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func BuildSliceOfPrimes(limit int) []int {
 	primesCh := GetPrimes(float64(limit))
 	primes := make([]int, 0)
@@ -36,4 +40,23 @@ func IsPrime(number int) bool {
 	}
 
 	return count == 0
+}
+
+func main() {
+	greater, result, primes := -1000, 0, BuildSliceOfPrimes(1000)
+	for a := -999; a < 1000; a += 2 {
+		for _, b := range primes {
+			n := 1
+
+			for IsPrime(n * n + a * n + b) {
+				n++
+			}
+
+			if n > greater {
+				greater, result = n, a * b
+			}
+		}
+	}
+
+	fmt.Println(result)
 }
