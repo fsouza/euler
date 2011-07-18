6770904ded68abf410b517fb5a95fb1971e6629e
Francisco Souza <francisco@franciscosouza.net>
2011-07-17 22:14:21 -0300

    Function that builds the matrix zeroed

--- problem_028/28.go
+++ problem_028/28.go
@@ -1,1 +1,18 @@
 package main
+
+type Row []int
+type Matrix []Row
+
+func BuildMatrix(rows int) Matrix {
+	if rows % 2 == 0 {
+		panic("I can't build matrixes with even lines")
+	}
+
+	matrix := make(Matrix, rows)
+
+	for i := 0; i < rows; i++ {
+		matrix[i] = make(Row, rows)
+	}
+
+	return matrix
+}
--- problem_028/28_test.go
+++ problem_028/28_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestRowType(t *testing.T) {
+	row := make(Row, 10)
+	if row[0] != 0 {
+		t.Errorf("Row type missing!")
+	}
+}
+
+
+func TestMatrixType(t *testing.T) {
+	matrix := make(Matrix, 10)
+	if matrix[0] != nil {
+		t.Errorf("Matrix type missing!")
+	}
+}
+
+func TestBuildMatrix(t *testing.T) {
+	matrix := BuildMatrix(3)
+	if matrix[0][0] != 0 {
+		t.Errorf("The matrix was built wrong")
+	}
+}
+
+
+func TestCanBuildMatrixOnlyForOddRowsAndCols(t *testing.T) {
+	defer func() {
+		if err := recover(); err == nil {
+			t.Errorf("Should fail trying to create a matrix with even number of columns/rows")
+		}
+	}()
+	BuildMatrix(4)
+}
+
