35647564dd310da3b8be271fdb122c0e844e89f3
Francisco Souza <francisco@franciscosouza.net>
2011-07-20 16:59:04 -0300

    Using recursive coin change algorithm to make test green

--- problem_031/31.go
+++ problem_031/31.go
@@ -6,7 +6,26 @@ func DivMod(divisor, numerator int) (int, int) {
 	return divisor / numerator, divisor % numerator
 }
 
-func CalculateDifferentWays(value int) (total int) {
-	totalOfCoins := len(coins)
-	return totalOfCoins
+func calculate(value, coinPosition int) int {
+	if value == 0 {
+		return 1
+	}
+
+	if value < 0 || coinPosition < 0 {
+		return 0
+	}
+
+	return calculate(value, coinPosition - 1) + calculate(value - coins[coinPosition], coinPosition)
+}
+
+func CalculateDifferentWays(value int) int {
+	i := 0
+
+	for i = len(coins) - 1; i >= 0; i-- {
+		if value / coins[i] > 0 {
+			break
+		}
+	}
+
+	return calculate(value, i)
 }
