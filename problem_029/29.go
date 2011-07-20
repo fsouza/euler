c8d37f70c779e6fd0afedcb5748dadf302830b43
Francisco Souza <francisco@franciscosouza.net>
2011-07-20 11:34:05 -0300

    Counting itens in sequence

--- problem_029/29.go
+++ problem_029/29.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"big"
+)
+
+func IsPresent(sequence []*big.Int, number *big.Int) bool {
+	for _, element := range sequence {
+		if element.Cmp(number) == 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func AddToSequence(sequence []*big.Int, numbers ...*big.Int) []*big.Int {
+	for _, number := range numbers {
+		if !IsPresent(sequence, number) {
+			sequence = append(sequence, number)
+		}
+	}
+
+	return sequence
+}
+
+func CountTermsInSequence(a, b int64) int {
+	sequence := make([]*big.Int, 0)
+	base := big.NewInt(a)
+	exponent := big.NewInt(b)
+
+	for i := big.NewInt(2); i.Cmp(base) < 1; i.Add(i, big.NewInt(1)) {
+		for j := big.NewInt(2); j.Cmp(exponent) < 1; j.Add(j, big.NewInt(1)) {
+			sequence = AddToSequence(sequence, big.NewInt(0).Exp(i, j, nil))
+		}
+	}
+
+	return len(sequence)
+}
--- problem_029/29_test.go
+++ problem_029/29_test.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"big"
+	"testing"
+)
+
+func TestIsPresent(t *testing.T) {
+	sequence := make([]*big.Int, 0)
+	sequence = AddToSequence(sequence, big.NewInt(0), big.NewInt(1))
+
+	if !IsPresent(sequence, big.NewInt(0)) {
+		t.Errorf("Should find 0 in the sequence")
+	}
+}
+
+
+func TestAddToSequence(t *testing.T) {
+	sequence := make([]*big.Int, 0)
+	sequence = AddToSequence(sequence, big.NewInt(0), big.NewInt(1))
+	sequence = AddToSequence(sequence, big.NewInt(0))
+
+	if len(sequence) != 2 {
+		t.Errorf("Length of sequence should be 2, but was %d.", len(sequence))
+	}
+}
+
+
+func TestFindTermsCountInSequence(t *testing.T) {
+	expected := 15
+	got := CountTermsInSequence(5, 5)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
