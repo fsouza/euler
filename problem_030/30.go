662a33a74a91da09d1d4f3bc3366a4105b2f45ce
Francisco Souza <francisco@franciscosouza.net>
2011-07-20 12:07:36 -0300

    Sum of digits of a number

--- problem_030/30.go
+++ problem_030/30.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"strconv"
+)
+
+func SumDigits(number int) (sum int) {
+	digits := strconv.Itoa(number)
+
+	for _, digit := range digits {
+		sum += digit - '0'
+	}
+
+	return sum
+}
+
+func SumOfPowersOfDigits(exponent int) int {
+	return 0
+}
--- problem_030/30_test.go
+++ problem_030/30_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestSumDigitsOfANumber(t *testing.T) {
+	expected := 6
+	got := SumDigits(123)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
+func TestSumOfPowersOfDigits(t *testing.T) {
+	expected := 19316
+	got := SumOfPowersOfDigits(4)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
