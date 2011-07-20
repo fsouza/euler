530ffe69ef87ce170b7be4f7a613c6c6194980cb
Francisco Souza <francisco@franciscosouza.net>
2011-07-20 15:45:29 -0300

    DivMod function and function to calculate different ways (not done yet)

--- problem_031/31.go
+++ problem_031/31.go
@@ -1,1 +1,12 @@
 package main
+
+var coins = []int{ 1, 2, 5, 10, 20, 50, 100, }
+
+func DivMod(divisor, numerator int) (int, int) {
+	return divisor / numerator, divisor % numerator
+}
+
+func CalculateDifferentWays(value int) int {
+	totalOfCoins := len(coins)
+	return totalOfCoins
+}
--- problem_031/31_test.go
+++ problem_031/31_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"testing"
+)
+
+type WaysTest struct {
+	in, out int
+}
+
+var WaysTests = []WaysTest{
+	WaysTest{ 2, 2, },
+	WaysTest{ 5, 4, },
+}
+
+func TestWays(t *testing.T) {
+	for _, test := range WaysTests {
+		expected := test.out
+		got := CalculateDifferentWays(test.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+		}
+	}
+}
+
+func TestDivMod(t *testing.T) {
+	expectedDiv, expectedMod := 5, 2
+	gotDiv, gotMod := DivMod(17, 3)
+
+	if expectedDiv != gotDiv {
+		t.Errorf("Assertion error, expected div to be %v, but was %v.", expectedDiv, gotDiv)
+	}
+
+	if expectedMod != gotMod {
+		t.Errorf("Assertion error, expected mod to be %v, but was %v.", expectedMod, gotMod)
+	}
+}
+
