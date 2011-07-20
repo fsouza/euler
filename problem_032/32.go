d77eb46d779ea4e3e725ad1b43466f9d0f2abce8
Francisco Souza <francisco@franciscosouza.net>
2011-07-20 17:44:02 -0300

    Basic verification if the set is pandigital by the length of multiplier and multiplicand

--- problem_032/32.go
+++ problem_032/32.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"strconv"
+)
+
+func HasPandigitalProduct(multiplier, multiplicand int) bool {
+	strMultiplier := strconv.Itoa(multiplier)
+	strMultiplicand := strconv.Itoa(multiplicand)
+
+	if len(strMultiplier) < 3 && len(strMultiplicand) < 3 {
+		return false
+	}
+
+	if len(strMultiplier) < 2 || len(strMultiplicand) < 2 {
+		return false
+	}
+
+	return true
+}
--- problem_032/32_test.go
+++ problem_032/32_test.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestIfTheMultiplicandAndMultiplierHasLessThan5DigitsTogetherTheProductIsntPanDigital(t *testing.T) {
+	type SmallMultiplierTest struct {
+		multiplier, multiplicand int
+	}
+
+	SmallMultiplierTests := []SmallMultiplierTest{
+		SmallMultiplierTest { 2, 4, },
+		SmallMultiplierTest { 2, 40, },
+		SmallMultiplierTest { 2, 999, },
+		SmallMultiplierTest { 20, 40, },
+		SmallMultiplierTest { 99, 99, },
+	}
+
+	for _, test := range SmallMultiplierTests {
+		if HasPandigitalProduct(test.multiplier, test.multiplicand) {
+			t.Errorf("%v x %v should not generate a pandigital product", test.multiplier, test.multiplicand)
+		}
+	}
+}
