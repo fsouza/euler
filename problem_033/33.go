465a38109e96ad4121e3d3e4d694934b28f9fd04
Francisco Souza <francisco@franciscosouza.net>
2011-07-21 15:27:44 -0300

    Starting to solve the problem

--- problem_033/33.go
+++ problem_033/33.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"strconv"
+)
+
+func IsNonTrivial(numerator, denominator int) bool {
+	strNumerator := strconv.Itoa(numerator)
+	strDenominator := strconv.Itoa(denominator)
+
+	if len(strNumerator) != 2 || len(strDenominator) != 2 {
+		return false
+	}
+
+	if strNumerator[0] == strDenominator[1] {
+		return float64(numerator) / float64(denominator) == float64(strNumerator[1] - '0') / float64(strDenominator[0] - '0')
+	}
+
+	if strNumerator[1] == strDenominator[0] {
+		return float64(numerator) / float64(denominator) == float64(strNumerator[0] - '0') / float64(strDenominator[1] - '0')
+	}
+
+	return false
+}
--- problem_033/33_test.go
+++ problem_033/33_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"testing"
+)
+
+type IsNonTrivialTest struct {
+	numerator, denominator int
+	out bool
+}
+
+var	IsNonTrivialTests = []IsNonTrivialTest {
+	IsNonTrivialTest{ 30, 50, false, },
+	IsNonTrivialTest{ 49, 98, true, },
+}
+
+func TestIsNonTrivial(t *testing.T) {
+	for _, test := range IsNonTrivialTests {
+		expected := test.out
+		got := IsNonTrivial(test.numerator, test.denominator)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected %v / %v to be trivial? %v.\nGot: %v.", test.numerator, test.denominator, expected, got)
+		}
+	}
+}
+
