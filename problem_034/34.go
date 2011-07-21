05db031c02094e71a0fc629db2f46cbe127853e9
Francisco Souza <francisco@franciscosouza.net>
2011-07-21 16:05:56 -0300

    Imported Factorial function from problem 20

--- problem_034/34.go
+++ problem_034/34.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"big"
+)
+
+// Function imported from problem 20 (where it's tested)
+func Factorial(number int64) *big.Int {
+	factorial := big.NewInt(1)
+
+	for i := number; i >= 1; i-- {
+		factorial.Mul(factorial, big.NewInt(i))
+	}
+
+	return factorial
+}
