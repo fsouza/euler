e6075ad1f800ce654c069f7320a33ea0f47ae522
Francisco Souza <francisco@franciscosouza.net>
2011-07-22 17:00:52 -0300

    Binary conversion

--- problem_036/36.go
+++ problem_036/36.go
@@ -1,1 +1,29 @@
 package main
+
+func Reverse(slice []int) []int {
+	length, half := len(slice), len(slice) / 2
+
+	for i := 0; i < half; i++ {
+		slice[i], slice[length - i - 1] = slice[length - i - 1], slice[i]
+	}
+
+	return slice
+}
+
+func DivMod(numerator, denominator int) (int, int) {
+	return numerator / denominator, numerator % denominator
+}
+
+func ToBinary(number int) string {
+	digits := []int{}
+
+	div, mod := DivMod(number, 2)
+	digits = append(digits, mod + '0')
+	for div > 1 {
+		div, mod = DivMod(div, 2)
+		digits = append(digits, mod + '0')
+	}
+	digits = append(digits, div + '0')
+
+	return string(Reverse(digits))
+}
--- problem_036/36_test.go
+++ problem_036/36_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	expected := "123"
+	out := Reverse([]int("321"))
+	got := string(out)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
+type ToBinaryTest struct {
+	in int
+	out string
+}
+
+var ToBinaryTests = []ToBinaryTest{
+	ToBinaryTest{ 1, "1", },
+	ToBinaryTest{ 2, "10", },
+	ToBinaryTest{ 3, "11", },
+	ToBinaryTest{ 5, "101", },
+	ToBinaryTest{ 15, "1111", },
+}
+
+func TestToBinary(t *testing.T) {
+	expected := "110"
+	got := ToBinary(6)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
+func TestDivMod(t *testing.T) {
+	expectedDiv, expectedMod := 2, 1
+	gotDiv, gotMod := DivMod(5, 2)
+	if expectedDiv != gotDiv {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expectedDiv, gotDiv)
+	}
+
+	if expectedMod != gotMod {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expectedMod, gotMod)
+	}
+}
