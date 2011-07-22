cef82f173c969bced4fe01fea794de63d3b0cea7
Francisco Souza <francisco@franciscosouza.net>
2011-07-22 15:57:49 -0300

    Starting with some helper functions for problem 35

--- problem_035/35.go
+++ problem_035/35.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"strconv"
+)
+
+func IsPrime(number int) bool {
+	if number < 0 {
+		number *= -1
+	}
+
+	if number < 4 {
+		return true
+	}
+
+	if number % 2 == 0 {
+		return false
+	}
+
+	count := 0
+	for i := 5; i < number; i += 2 {
+		if number % i == 0 {
+			count++
+		}
+	}
+
+	return count == 0
+}
+
+func Rotate(number int) int {
+	numberDigits := []int(strconv.Itoa(number))
+	length := len(numberDigits)
+
+	the_digit := numberDigits[0]
+	for i := length - 1; i >= 0; i-- {
+		the_digit, numberDigits[i] = numberDigits[i], the_digit
+	}
+
+	newNumber, _ := strconv.Atoi(string(numberDigits))
+	return newNumber
+}
+
+func IsCircular(number int) bool {
+	rotated := Rotate(number)
+	for rotated != number {
+		if !IsPrime(rotated) {
+			return false
+		}
+		rotated = Rotate(rotated)
+	}
+
+	return true
+}
--- problem_035/35_test.go
+++ problem_035/35_test.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"testing"
+)
+
+type IsCircularTest struct {
+	in int
+	out bool
+}
+
+var IsCircularTests = []IsCircularTest {
+	IsCircularTest{ 17, true, },
+	IsCircularTest{ 19, false, },
+	IsCircularTest{ 23, false, },
+	IsCircularTest{ 40, false, },
+	IsCircularTest{ 197, true, },
+}
+
+func TestIsCircular(t *testing.T) {
+	for _, test := range IsCircularTests {
+		expected := test.out
+		got := IsCircular(test.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected %v to be circular? %v.\nGot: %v.", test.in, expected, got)
+		}
+	}
+}
+
+func TestRotate(t *testing.T) {
+	expected := 971
+	got := Rotate(197)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
