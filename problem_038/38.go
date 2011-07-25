4df0b34dc09a9912cc00e3d78f03fd99fd8dbaa7
Francisco Souza <francisco@franciscosouza.net>
2011-07-25 20:40:29 -0300

    IsPandigital function

--- problem_038/38.go
+++ problem_038/38.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"sort"
+)
+
+func IsPandigital(input string) bool {
+	sequence := []int{ '1', '2', '3', '4', '5', '6', '7', '8', '9', }
+	digits := []int(input)
+	sort.Ints(digits)
+
+	if len(digits) == len(sequence) {
+		for i, digit := range digits {
+			if digit != sequence[i] {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
--- problem_038/38_test.go
+++ problem_038/38_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"testing"
+)
+
+type IsPandigitalTest struct {
+	in string
+	out bool
+}
+
+var IsPandigitalTests = []IsPandigitalTest{
+	IsPandigitalTest{ "123", false, },
+	IsPandigitalTest{ "123456789", true, },
+	IsPandigitalTest{ "192384576", true, },
+	IsPandigitalTest{ "192984576", false, },
+}
+
+func TestIsPandigital(t *testing.T) {
+	for _, test := range IsPandigitalTests {
+		expected := test.out
+		got := IsPandigital(test.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected %v to be pandigital? %v.\nGot: %v.", test.in, expected, got)
+		}
+	}
+}
+
