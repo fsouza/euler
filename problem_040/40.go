99aa04f39d19b468c6870b1668064e2ac5d51e0f
Francisco Souza <francisco@franciscosouza.net>
2011-07-25 21:50:26 -0300

    solution for problem 40

--- problem_040/40.go
+++ problem_040/40.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -20,4 +21,16 @@ func BuildSequence(limit int) string {
 func FindNthDigit(position int) uint8 {
 	fractional := BuildSequence(position)
 	return fractional[position - 1] - '0'
+}
+
+func main() {
+	sequence := BuildSequence(1000000)
+	fmt.Println(
+		(sequence[0] - '0') *
+		(sequence[9] - '0') *
+		(sequence[99] - '0') *
+		(sequence[999] - '0') *
+		(sequence[9999] - '0') *
+		(sequence[99999] - '0') *
+		(sequence[999999] - '0'))
 }
