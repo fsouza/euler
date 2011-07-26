2ee8673e79b69d6cb4db6aa6bc6b0b09569a81e4
Francisco Souza <francisco@franciscosouza.net>
2011-07-25 21:22:37 -0300

    Finding the nth digit of the sequence

--- problem_040/40.go
+++ problem_040/40.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func FindNthDigit(position int) uint8 {
+	var fractional string
+	digits := make([]string, 0)
+
+	for number := 1; len(fractional) < position; number++ {
+		digits = append(digits, strconv.Itoa(number))
+		fractional = strings.Join(digits, "")
+	}
+
+	return fractional[position] - '0'
+}
--- problem_040/40_test.go
+++ problem_040/40_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestFindNthDigitOfTheFractionalPart(t *testing.T) {
+	expected := uint8(1)
+	got := FindNthDigit(12)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
