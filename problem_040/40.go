b56dfabf2c850434485fd1504620fd0a5109f0d8
Francisco Souza <francisco@franciscosouza.net>
2011-07-25 21:23:53 -0300

    Refactoring

--- problem_040/40.go
+++ problem_040/40.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func FindNthDigit(position int) uint8 {
+func BuildSequence(position int) string {
 	var fractional string
 	digits := make([]string, 0)
 
@@ -14,5 +14,13 @@ func FindNthDigit(position int) uint8 {
 		fractional = strings.Join(digits, "")
 	}
 
+	return fractional
+}
+
+func FindNthDigit(position int) uint8 {
+	fractional := BuildSequence(position)
 	return fractional[position] - '0'
+}
+
+func main() {
 }
