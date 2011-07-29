3551b51f20fa6a5d33006faba513e94ab746bf32
Francisco Souza <francisco@franciscosouza.net>
2011-07-28 21:16:48 -0300

    Function that checks if a number is pandigital

--- problem_041/41.go
+++ problem_041/41.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"sort"
+	"strconv"
+)
+
+func IsPandigital(number int) bool {
+	digits := []int(strconv.Itoa(number))
+	sort.Ints(digits)
+
+	sequence := make([]int, len(digits))
+	for i, counter := digits[0], 0; i < digits[0] + len(digits); i, counter = i + 1, counter + 1 {
+		sequence[counter] = i
+	}
+
+	for i, digit := range digits {
+		if digit != sequence[i] {
+			return false
+		}
+	}
+
+	return true
+}
--- problem_041/41_test.go
+++ problem_041/41_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"testing"
+)
+
+type IsPandigitalTest struct {
+	in int
+	out bool
+}
+
+var	IsPandigitalTests = []IsPandigitalTest{
+	IsPandigitalTest{2134, true},
+	IsPandigitalTest{2445, false},
+	IsPandigitalTest{123456789, true},
+}
+
+func TestIsPandigital(t *testing.T) {
+	for _, test := range IsPandigitalTests {
+		expected := test.out
+		got := IsPandigital(test.in)
+		if expected != got {
+			t.Errorf("Assertion error.\nExpected %v to be pandigital? %v.\nGot: %v.", test.in, expected, got)
+		}
+	}
+}
+
