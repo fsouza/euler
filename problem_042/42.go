71549751b9b7a242875a19bc2ad67ca47deb5ebd
Francisco Souza <francisco@franciscosouza.net>
2011-08-03 16:40:09 -0300

    Building list of triangle numbers

--- problem_042/42.go
+++ problem_042/42.go
@@ -1,1 +1,13 @@
 package main
+
+func TriangleNumberAt(position int) int {
+	return position * (position + 1) / 2
+}
+
+func BuildListOfTriangleNumbers(limit int) []int {
+	list := make([]int, limit)
+	for i := 0; i < limit; i++ {
+		list[i] = TriangleNumberAt(i + 1)
+	}
+	return list
+}
--- problem_042/42_test.go
+++ problem_042/42_test.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestTriangleNumberAt(t *testing.T) {
+	expected := 45
+	got := TriangleNumberAt(9)
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
+
+func TestBuildListOfTriangleNumbers(t *testing.T) {
+	triangles := BuildListOfTriangleNumbers(10)
+	expected := 55
+	got := triangles[9]
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
