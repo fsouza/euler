a1cd46835925ccccd4642ab53b7553fc28ad9836
Francisco Souza <francisco@franciscosouza.net>
2011-08-03 16:42:04 -0300

    Imported function to count letter of number

--- problem_042/42.go
+++ problem_042/42.go
@@ -1,5 +1,15 @@
 package main
 
+func CountLetterNumbers(word string) (sum int) {
+	for _, c := range word {
+		if c >= 'A' && c <= 'Z' {
+			sum += int(c - 'A' + 1)
+		}
+	}
+
+	return
+}
+
 func TriangleNumberAt(position int) int {
 	return position * (position + 1) / 2
 }
--- problem_042/42_test.go
+++ problem_042/42_test.go
@@ -22,3 +22,11 @@ func TestBuildListOfTriangleNumbers(t *testing.T) {
 	}
 }
 
+func TestCanFindTheSumOfAWord(t *testing.T) {
+	expected := 53
+	got := CountLetterNumbers("COLIN")
+	if expected != got {
+		t.Errorf("Assertion error.\nExpected: %v.\nGot: %v.", expected, got)
+	}
+}
+
